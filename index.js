7ac16ca8f23798fe307ba599d5e63f2a3db0c29b
Thanh Tran <thanh.tran@loyaworld.com>
2019-10-21 23:48:36 +0700

    initital

---
+++ index.js
@@ -0,0 +1,7 @@
+/** @format */
+
+import {AppRegistry} from 'react-native';
+import MakeMoneyTogether from './src/App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => MakeMoneyTogether);
